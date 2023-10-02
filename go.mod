3f9ed44a7c3ab653f425e775deae5b249992c319
Bernard Muller <b.mullerjnr@gmail.com>
2023-10-02 18:48:56 +0200

    feat: FEN string

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bernardmuller/e4e5kf3OD
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	FEN := "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+	fmt.Println("Hello, World!")
+	fmt.Println(FEN)
+}
