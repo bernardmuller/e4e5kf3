0f04c5665a520414158c6d17d69bf63eb077614c
Bernard Muller <b.mullerjnr@gmail.com>
2023-10-02 19:05:22 +0200

    feat: build board from FEN string

--- main.go
+++ main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	FEN := "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-	fmt.Println("Hello, World!")
-	fmt.Println(FEN)
+	// FEN := "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+	FEN := "rnbqkbnr/ppp2ppp/3p4/4p3/3PP3/5N2/PPP2PPP/RNBQKB1R b KQkq d3 0 3"
+	board := strings.Split(FEN, "")
+	fmt.Println(board)
+
+	for i := 0; i < len(board); i++ {
+		if board[i] == "/" {
+			fmt.Print("\n")
+		} else if board[i] == "1" || board[i] == "2" || board[i] == "3" || board[i] == "4" || board[i] == "5" || board[i] == "6" || board[i] == "7" || board[i] == "8" {
+			fmt.Print(strings.Repeat(" ", int(board[i][0])-48))
+		} else {
+			fmt.Print(board[i])
+		}
+	}
 }
