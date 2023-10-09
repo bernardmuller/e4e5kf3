31533569ff7004d5b91b4ca45abe2d5225be3b20
Bernard <b.mullerjnr@gmail.com>
2023-10-09 18:56:35 +0200

    chore: display refinements

--- main.go
+++ main.go
@@ -25,19 +25,12 @@ var display = map[Piece]string{
 	"p": "♙",
 	"q": "♕",
 	"r": "♖",
+	"i": "\u2022",
+	"x": "\u2717",
+	"#": "\u2592",
 }
 
 type Square string
-
-var chessboard = [][]string{
-	{"a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"},
-	{"a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"},
-	{"a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"},
-	{"a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"},
-	{"a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"},
-	{"a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"},
-	{"a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"},
-}
 
 type File string
 type Rank string
@@ -124,18 +117,19 @@ func startGameLoop(board *[][]string) {
 	for {
 		current_board := *board
 		fmt.Print("\033c")
-		fmt.Println("   abcdefgh")
-		fmt.Println(" ╔══════════╗")
+		fmt.Println("   a b c d e f g h")
+		fmt.Println(" ╔═════════════════╗")
 		for i := 0; i < 8; i++ {
 			fmt.Print(8 - i)
 			fmt.Print("║ ")
 			for j := 0; j < 8; j++ {
 				fmt.Print(current_board[i][j])
+				fmt.Print(" ")
 			}
-			fmt.Print(" ║")
+			fmt.Print("║")
 			fmt.Println()
 		}
-		fmt.Println(" ╚══════════╝")
+		fmt.Println(" ╚═════════════════╝")
 
 		in := bufio.NewReader(os.Stdin)
 		fmt.Print("move (eg. e2 e4):")
